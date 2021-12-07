35b9e6a963f1729a41201a576dc6a44fcb3d2c1d
Fox Rigney <foxrigney@gmail.com>
2021-12-06 21:22:05 -0600

    bug fix

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -28,69 +28,66 @@ const LoginForm = () => {
       event.stopPropagation();
     }
 
-    // submit form
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    console.log(formState);
-    try {
-      const { data } = await login({
-        variables: { ...formState },
+    
+      try {
+        const { data } = await login({
+          variables: { ...formState },
+        });
+
+        Auth.login(data.login.token);
+      } catch (e) {
+        console.error(e);
+        setShowAlert(true);
+      }
+
+      // clear form values
+      setFormState({
+        email: '',
+        password: '',
       });
 
-      Auth.login(data.login.token);
-    } catch (e) {
-      console.error(e);
-      setShowAlert(true);
-    }
+    };
 
-    // clear form values
-    setFormState({
-      email: '',
-      password: '',
-    });
-  
+    return (
+      <>
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+          <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+            Something went wrong with your login credentials!
+          </Alert>
+          <Form.Group>
+            <Form.Label htmlFor='email'>Email</Form.Label>
+            <Form.Control
+              type='text'
+              placeholder='Your email'
+              name='email'
+              onChange={handleInputChange}
+              value={userFormData.email}
+              required
+            />
+            <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label htmlFor='password'>Password</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Your password'
+              name='password'
+              onChange={handleInputChange}
+              value={userFormData.password}
+              required
+            />
+            <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+          </Form.Group>
+          <Button
+            disabled={!(userFormData.email && userFormData.password)}
+            type='submit'
+            variant='success'>
+            Submit
+          </Button>
+        </Form>
+      </>
+    );
   };
 
-  return (
-    <>
-      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-          Something went wrong with your login credentials!
-        </Alert>
-        <Form.Group>
-          <Form.Label htmlFor='email'>Email</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Your email'
-            name='email'
-            onChange={handleInputChange}
-            value={userFormData.email}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label htmlFor='password'>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Your password'
-            name='password'
-            onChange={handleInputChange}
-            value={userFormData.password}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
-        </Form.Group>
-        <Button
-          disabled={!(userFormData.email && userFormData.password)}
-          type='submit'
-          variant='success'>
-          Submit
-        </Button>
-      </Form>
-    </>
-  );
-};
-
-export default LoginForm;
+  export default LoginForm;
