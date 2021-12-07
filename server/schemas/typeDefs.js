5fe135d158672d21840e660725d1923b64822884
Fox Rigney <foxrigney@gmail.com>
2021-12-06 19:37:35 -0600

    typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,26 +3,22 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
   type User {
     _id: ID
-    username: String
+    username: String!
     email: String
-    password: String
-    thoughts: [Thought]!
+    bookCount: Int
+    savedBooks: [Book]
   }
 
-  type Thought {
-    _id: ID
-    thoughtText: String
-    thoughtAuthor: String
-    createdAt: String
-    comments: [Comment]!
+  type Book {
+    bookId: ID!
+    title: String!
+    description: String
+    image: String
+    link: String
+    authors: [String]
   }
 
-  type Comment {
-    _id: ID
-    commentText: String
-    commentAuthor: String
-    createdAt: String
-  }
+  
 
   type Auth {
     token: ID!
@@ -30,20 +26,17 @@ const typeDefs = gql`
   }
 
   type Query {
-    users: [User]
-    user(username: String!): User
-    thoughts(username: String): [Thought]
-    thought(thoughtId: ID!): Thought
     me: User
   }
 
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    addThought(thoughtText: String!): Thought
-    addComment(thoughtId: ID!, commentText: String!): Thought
-    removeThought(thoughtId: ID!): Thought
-    removeComment(thoughtId: ID!, commentId: ID!): Thought
+
+   saveBook(authors:[String]!, bookId: ID!, title: String!, description: String!, image: String!,link: String)!: User
+
+    removeBook(bookId: ID!): User
+    
   }
 `;
 
