65f484ccc6517f23819ab31e80236736863cd65a
Fox Rigney <foxrigney@gmail.com>
2021-12-06 20:43:17 -0600

    i got der mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,70 @@
+import { gql } from '@apollo/client';
+
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($authors:[String]!, $bookId: ID!, $title: String!, $description: String!, $image: String!,$link: String!) {
+    saveBook(authors: $authors ,description: $description, title: $title,bookId: $bookId,image: $image, link: $link
+) {
+    _id
+    username
+    email
+    bookCount
+   savedBooks {
+      bookId
+      title
+      description
+      image
+      link
+      authors
+      }
+    }
+
+    
+  }
+`;
+
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook( $bookId: ID!) {
+    removeBook(bookId: $bookId) {
+    _id
+    username
+    email
+    bookCount
+   savedBooks {
+      bookId
+      title
+      description
+      image
+      link
+      authors
+      }
+    }
+
+    
+  }
+`;
